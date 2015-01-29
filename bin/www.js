3a9890385714b36bfb7056af82a1d3366b77248e
Joel <joel_1st@hotmail.com>
2015-01-30 04:09:21 +1000

    update to port 80

--- bin/www.js
+++ bin/www.js
@@ -3,7 +3,7 @@ var debug = require('debug')('bronze');
 var app = require('../app');
 var db = require('../db');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
